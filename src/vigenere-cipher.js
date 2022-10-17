bcc5eb316b1e809fe0d44d9a7c4c2c6c9e04da76
Lada <100614341+ladaln@users.noreply.github.com>
2022-10-17 08:57:00 +0300

    add: ready

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  reverse = true
+
+  constructor(rev = true) {
+    this.reverse = rev
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(!message 
+      || typeof message != 'string' 
+      || message.length == 0 
+      || !key || typeof key != 'string'
+      || key.length == 0
+    ){
+      throw new Error('Incorrect arguments!')
+    }
+    key = key.toUpperCase()
+    let ni = 0
+    let res = message.toUpperCase().split('').map((char, i) => {
+      console.log(`message[${i}]=${char} key[${ni % key.length}]=${key[i % key.length]}`)
+      if (char.charCodeAt(0) < 65 || char.charCodeAt(0) > 90) return char
+      ni++
+      let symbNumber = char.charCodeAt(0) - 65 // A - 1, Z - 26
+      let keyNumber = key[(ni -1) % key.length].charCodeAt(0) - 65 // A - 0, Z - 25
+      let newChar = (symbNumber + keyNumber) % 26
+      return String.fromCharCode( newChar + 65 )
+    }).join('')
+    return this.reverse ? res : res.split('').reverse().join('')
+  }
+  decrypt(message, key) {
+    if(!message 
+      || typeof message != 'string' 
+      || message.length == 0 
+      || !key || typeof key != 'string'
+      || key.length == 0
+    ){
+      throw new Error('Incorrect arguments!')
+    }
+    key = key.toUpperCase()
+    let ni = 0
+    let res =  message.toUpperCase().split('').map((char, i) => {
+      if (char.charCodeAt(0) < 65 || char.charCodeAt(0) > 90) return char
+      ni++
+      let symbNumber = char.charCodeAt(0) - 65 // A - 1, Z - 26
+      let keyNumber = key[(ni -1) % key.length].charCodeAt(0) - 65 // A - 0, Z - 25
+      let newChar = (symbNumber - keyNumber) 
+      newChar = newChar < 0 ? newChar + 26 : newChar
+      newChar = newChar % 26
+      return String.fromCharCode( newChar + 65 )
+    }).join('')
+    return this.reverse ? res : res.split('').reverse().join('')
   }
 }
 
