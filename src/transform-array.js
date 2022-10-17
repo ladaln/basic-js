487e7d9b72fe09a2b3c2786f1cfa655fb8ba9640
Lada <100614341+ladaln@users.noreply.github.com>
2022-10-17 08:56:29 +0300

    add: ready

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr1) {
+  if(!arr1 || typeof arr1 !== 'object') throw new Error("'arr' parameter must be an instance of the Array!")
+  let arr = JSON.parse(JSON.stringify(arr1))
+  try {
+    i = 0;
+    while (i < arr.length) {
+      if(!arr[i]) arr = arr.filter((it, index) => index != i )
+      if(arr[i] === '--double-next' && i < arr.length - 1 && isNumber(arr[i+1])) { 
+        arr[i] = arr[i+1]
+      }
+      if(arr[i] === '--double-prev' && i > 0 && isNumber(arr[i-1])) { 
+        arr[i] = arr[i-1]
+      }
+      if(arr[i] === '--discard-prev' && i != 0) { arr = arr.filter((it, index) => index != i - 1 ); i-- }
+      if(arr[i] === '--discard-next' && i != arr.length + 1) { arr = arr.filter((it, index) => index != i + 1 ); }
+      i++
+    }
+    return arr.filter(it => isNumber(it))
+  } catch(e) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  return arr
 }
+
+const vals = new Set(['--discard-prev', '--discard-next', '--double-prev', '--double-next'])
+const isNumber = (num) => vals.has(num) ? false : true
 
 module.exports = {
   transform
