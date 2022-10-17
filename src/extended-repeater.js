d75c2b435c869d60442290448fd71199d2295a6c
Lada <100614341+ladaln@users.noreply.github.com>
2022-10-17 08:49:41 +0300

    add: readfy to check

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr = ''
+  const keys = new Set( Object.keys(options) )
+  const times = keys.has('repeatTimes') ? options.repeatTimes : 1
+  const separator = keys.has('separator') ? options.separator : '+'
+  const addition = keys.has('addition') ? options.addition : ''
+  const aTimes = keys.has('additionRepeatTimes') ? options.additionRepeatTimes : 1
+  const aSeparator = keys.has('additionSeparator') ? options.additionSeparator : '|'
+
+  for(i = 0; i < times; i++) {
+    newStr += str
+    for (let j = 0; j < aTimes; j++){
+      newStr += j < aTimes - 1 ? addition + aSeparator : addition
+    }
+    newStr += i < times - 1 ? separator : ''
+  }
+  return newStr
 }
 
 module.exports = {
