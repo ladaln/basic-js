f2f7bc701315eb05db49fab8b62cc7733880194d
Lada <100614341+ladaln@users.noreply.github.com>
2022-10-17 08:47:59 +0300

    add: ready

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let allDoms = domains.map(domain =>
+    domain.split('.').reverse().map((it, i, ar) => {
+      let str = ''
+      for(let j = 0; j <= i; j++) {
+        str += `.${ar[j]}`
+      }
+      return str
+    })
+  )
+  //let maxLength = allDoms.sort((a, b) => b.length - a.length)[0]
+  let obj = {}
+  for(let i = 0; i < allDoms.length; i++) {
+    for(let j = 0; j < allDoms[i].length; j++) {
+      !obj[allDoms[i][j]] ? obj[allDoms[i][j]] = 1 : obj[allDoms[i][j]]++
+    }
+  }
+  return obj
 }
 
 module.exports = {
