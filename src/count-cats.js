dc16f1ef1f7801d39e92bd8e00e3ba3e2b093734
Lada <100614341+ladaln@users.noreply.github.com>
2022-10-17 08:47:07 +0300

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,19 +1,33 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-
-function countCats(counCats) {
-  let number
-  throw new NotImplementedError('Not implemented');
- let newArray = countCats.filter (function (cat)=>{
-if (cat==="^^"){
- return cat}
-else{
-return 0}
-}
-)
-if(newArray[0]===0){
-return 0}
- else return let number= newArray.length()
+/**
+ * Given matrix where you have to find cats by ears "^^"
+ *
+ * @param {Array<Array>} matrix 
+ * @return {Number} count of cats found
+ *
+ * @example
+ * countCats([
+ *  [0, 1, '^^'],
+ *  [0, '^^', 2],
+ *  ['^^', 1, 2]
+ * ]) => 3`
+ *
+ */
+function countCats(/* matrix */) {
+  if([...arguments].length == 0) return 0;
+  let count = 0;
+  [...arguments].forEach(it => {
+    if(typeof it == 'object') {
+      if(it!=null && it.length != 0)
+        it.forEach(el => {
+          count += countCats(el);
+        })
+    } else {
+      count += it == '^^' ? 1 : 0
+    }
+  })
+  return count
 }
 
 module.exports = {
